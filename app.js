336dc0b004890f2a4272f3e0596b3c5eeb3611b4
arpit-sat <admin@arpitsatnalika.in>
2020-08-31 14:10:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
