f4f92cebb404d1f6d8da7585a2d18dbacdf29cd2
arpit-sat <admin@arpitsatnalika.in>
2020-09-02 11:46:27 +0530

    Added path.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
