4cd03905a8ce7102a8ae8a7afc7dc55a88068f94
arpit-sat <admin@arpitsatnalika.in>
2020-09-08 12:54:16 +0530

    Updated Backend

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const rootDir = require('../util/path')
-
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
